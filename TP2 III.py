3112daa518b78db4261c603e28fac160fc2afe99
22406787 <ahachicha4@outlook.fr>
2025-12-19 12:44:19 +0100

    19/12/2025

--- TP2 III.py
+++ TP2 III.py
@@ -3,3 +3,14 @@ def binaire(n):
 
 
 print(binaire(5))
+
+def reste_puissance(x,p,n):
+    p_bin=binaire(p)
+    reste=1
+    for i in range(len(p_bin)):
+        if p_bin[len(p_bin)-1-i] == '1':
+            reste*=(x**(2**i))%n
+    reste=reste%n
+    return reste
+
+print(reste_puissance(2,17,55))
